1bc1ba243ead274bf84a4906cd45264f558457c9
Alexis <alexis25.py@gmail.com>
2021-01-04 02:25:13 +0100

    Reset tables

--- fixtures.sql
+++ fixtures.sql
@@ -1,16 +1,10 @@
-DROP TABLE IF EXISTS RDV;
-DROP TABLE IF EXISTS TYPE_SERVICE;
-
-INSERT INTO TYPE_SERVICE(libelle) VALUES
-	('typeUn'),
-	('typeDeux');
-
-INSERT INTO RDV(date_rdv,heure,type_service_id) VALUES
-	('2020-12-29','08:00',1),
-	('2020-12-29','08:30',1),
-	('2020-12-29','10:00',2),
-	('2020-12-29','13:00',1),
-	('2020-12-29','14:30',2);
-
-insert into USER (username,roles, password) values('client','{"ROLE_ADMIN"}', "passwordClient");
-INSERT INTO `user`(`username`, `roles`, `password`) VALUES ('client','{"roles":"ROLE_ADMIN"}', "passwordClient")
+set foreign_key_checks = 0;
+truncate table user;
+truncate table carousel_like;
+truncate table media;
+truncate table accounting;
+truncate table type_service;
+truncate table type_produit;
+truncate table category_accounting;
+truncate table rdv;
+truncate table produit;
