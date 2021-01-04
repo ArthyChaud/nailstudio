8a0882fc8e48844eff68251a59984b9f0bcde347
Alexis <alexis25.py@gmail.com>
2021-01-04 02:27:53 +0100

    Route change

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -16,20 +16,10 @@ class IndexController extends AbstractController
     {
         if($this->isGranted('ROLE_ADMIN')) {
             return $this->redirectToRoute('accueil');
-            // return $this->render('admin/produit/showProduits.html.twig');
         }
         if($this->isGranted('ROLE_CLIENT')) {
-            /*            if(! is_null($this->getUser())){
-                            echo "<br>";
-                            echo " id: ".$this->getUser()->getId();
-                            echo " roles :   ";
-                            print_r($this->getUser()->getRoles());
-                            die();
-                        }*/
             return $this->redirectToRoute('rdv_show');
-            //  return $this->render('client/boutique/produit.html.twig');
         }
-        return $this->redirectToRoute('rdv_show');
-
+        return $this->redirectToRoute('accueil');
     }
 }
